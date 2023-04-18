fb201899081038ab26f109f03d03a16b1f3e1f0d
ktesibios285 <ktesibios285@gmail.com>
2023-04-17 21:34:13 -0500

    webpack-starter

---
+++ webpack-starter/src/generateQuote.js
@@ -0,0 +1,5 @@
+function generateQuote() {
+    return "Hello World! GOOD TO SEE YOU AGAIN!";
+}
+
+export default generateQuote;
---
+++ webpack-starter/src/index.js
@@ -0,0 +1,4 @@
+import generateQuote from "./generateQuote";
+import "./styles/main.scss";
+
+console.log(generateQuote());
